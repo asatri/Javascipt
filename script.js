bb875eb830a7f724e1a4e94cb393d27285bc24da
asatri <39822029+asatri@users.noreply.github.com>
2018-06-11 20:08:41 +0300

    Създаваме var-ове за Главната снимка (Screen)
    Създаваме var-ове:
    
    Главната снимка -Screen 
    Caption -> Caption 
    Info - Info 
    Thumbnails

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+var screen = document.getElementById('screen');
+var caption = document.getElementById('caption');
+var info = document.getElementById('info');
+var thumbnails = document.getElementById('thumbnails');
