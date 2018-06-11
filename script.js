c7223f027764b36f5d73dec3111ee6d210200735
asatri <39822029+asatri@users.noreply.github.com>
2018-06-11 20:15:22 +0300

    Добавяме function - InitGallery
    Тази Функцаия load-ва снимките в Thumbs. Също така сменя размера на 170px. Ако клкнете на сниката – пуска function- changeImage

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var screen = document.getElementById('screen');
 var caption = document.getElementById('caption');
 var info = document.getElementById('info');
 var thumbnails = document.getElementById('thumbnails');
+
+function initGallery() {
+	for(var i = 0; i < images.length; i++) {
+		var image = images[i];
+		var img = document.createElement('img');
+		img.src = 'images/thumbs/thumb_' + images[i].url;
+		img.setAttribute('width', '170');
+		img.setAttribute('data-index', i);
+		img.addEventListener('click', changeImage);
+		thumbnails.appendChild(img);
+	}
+
+
+};
